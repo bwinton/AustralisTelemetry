fb711a2cd0cab3ca664eee1100966d8b4adb68f0
Blake Winton <bwinton@latte.ca>
2014-02-21 09:58:42 -0500

    Add comments to run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
 python -m mapreduce.job ../uitour.py --input-filter ../filter-test.json --num-mappers 16 --num-reducers 4 --data-dir /mnt/telemetry/work --work-dir /mnt/telemetry/work --output /mnt/telemetry/my_mapreduce_results.out --bucket "telemetry-published-v1"
+
+# if /cache exists --data-dir /mnt/telemetry/cache --local-only
