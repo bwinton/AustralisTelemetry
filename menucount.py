b63b48463571145a03560a7550819674d288ef25
Blake Winton <bwinton@latte.ca>
2014-03-26 18:21:41 -0400

    Update menucount.py to log more of the data we're interested in. And hopefully prevent the problems we've seen!

--- menucount.py
+++ menucount.py
@@ -1,6 +1,67 @@
 import json
+import sys
+import traceback
 
 # Check click-menu-button vs click-appmenu usage.
+
+def getButtonData(data):
+  rv = None
+  if "left" in data:
+    left = data["left"]
+    if left:
+      return {
+        "length": 1,
+        "count": left,
+      }
+  return rv
+
+def handleMultipleButtons(event):
+  rv = []
+  for key in event:
+    data = getButtonData(event[key])
+    if data:
+      rv.append([key, data])
+  return rv
+
+def handleSingleButton(event):
+  rv = []
+  data = getButtonData(event["button"])
+  if data:
+    rv.append([None, data])
+  return rv
+
+def handleDirectNumbers(event):
+  rv = []
+  for key in event:
+    rv.append([key, {
+      "length": 1,
+      "count": event[key],
+    }])
+  return rv
+
+
+def defaultHandler(event):
+  return None
+
+handlers = {
+  "click-bookmarks-menu-button": handleMultipleButtons,
+  "click-menu-button": handleSingleButton,
+  "click-builtin-item": handleMultipleButtons,
+  "customize": handleDirectNumbers
+}
+
+def handleEvents(events, cx, uptime, version):
+  for event in events:
+    handler = handlers.get(event, defaultHandler)
+    rv = handler(events[event])
+    if rv:
+      for extra, data in rv:
+        key = event
+        if extra:
+          key += "-" + extra
+        key += "-" + version
+        data["uptime"] = uptime
+        cx.write(key, json.dumps(data))
 
 def map(k, d, v, cx):
   try:
@@ -11,30 +72,29 @@ def map(k, d, v, cx):
     if not "UITelemetry" in s:
       return
     ui = s["UITelemetry"]
-    countableEventBuckets = []
-    customizeBuckets = []
+    countableEvents = {}
     if "toolbars" in ui:
+      cx.write("session-count-" + j["info"]["appVersion"], 1)
+      cx.write("session-uptime-" + j["info"]["appVersion"], s["uptime"])
       toolbars = ui["toolbars"]
       if "countableEvents" in toolbars:
         countableEvents = toolbars["countableEvents"]
-    for bucket in countableEvents:
-      events = countableEvents[bucket]
-      for event in events:
-        if "button" in events[event] and "left" in events[event]["button"]:
-          data = {
-            "length": 1,
-            "count": events[event]["button"]["left"],
-            "uptime": s["uptime"]
-          }
-          cx.write(event + "-" + j["info"]["appVersion"], data)
-
+        for bucket in countableEvents:
+          handleEvents(countableEvents[bucket], cx,
+            s["uptime"], j["info"]["appVersion"])
   except Exception, e:
-    cx.write("JSON PARSE ERROR:", e)
+    print >> sys.stderr, "BW2", e
+    print >> sys.stderr, traceback.format_exc()
+    cx.write("JSON PARSE ERROR:", str(e))
 
 def combine(k, v, cx):
   if k == "JSON PARSE ERROR:":
     for i in set(v):
       cx.write(k, i)
+    return
+
+  if k.startswith("session-"):
+    cx.write(k, sum(v))
     return
   # v = [{length: 1, count: 123, uptime: 456}, {length: 1, count: 789, uptime: 101112}, ...]
 
@@ -44,25 +104,37 @@ def combine(k, v, cx):
     "uptime": 0
   }
   for item in v:
+    # print >> sys.stderr, "BW3", item
+    item = json.loads(item)
     summary["length"] += item["length"]
     summary["count"] += item["count"]
     summary["uptime"] += item["uptime"]
-  cx.write(k, summary)
+  cx.write(k, json.dumps(summary))
 
 def reduce(k, v, cx):
   if k == "JSON PARSE ERROR:":
     for i in set(v):
       cx.write(k, i)
     return
+
+  if k.startswith("session-"):
+    cx.write(k, sum(v))
+    return
+
   # v = [{length: 3, count: 123, uptime: 456}, {length: 50, count: 789, uptime: 101112}, ...]
   lengths = 0
   counts = 0
   uptime = 0
   for item in v:
+    # print >> sys.stderr, "BW4", item
+    item = json.loads(item)
     lengths += item["length"]
     counts += item["count"]
     uptime += item["uptime"]
   cx.write(k + "-length", lengths)
   cx.write(k + "-count", counts)
   cx.write(k + "-uptime", uptime)
-  cx.write(k + "-scaled", counts * 1.0 / uptime)
+  if uptime > 0:
+    cx.write(k + "-scaled", counts * 1.0 / uptime)
+  else:
+    cx.write(k + "-scaled", "Divide by 0");
