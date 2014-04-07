197d1ac671d52831ea836490fc711b743bbb3060
Blake Winton <bwinton@latte.ca>
2014-04-07 10:26:03 -0400

    Add an output argument.

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 # Set up arguments.
 KEY=""
 SECRET=""
@@ -5,6 +6,7 @@ FILTER="filter-test.json"
 MAPREDUCE="uitour"
 VERBOSE=false
 PULL="--local-only"
+OUTPUT="my_mapreduce_results.out"
 
 # Pull is a little more complicated, since we only want to do it if we have cached data.
 if [ -d "../work/cache/saved_session" ]; then
@@ -30,6 +32,7 @@ while [[ $1 ]]; do
       echo "  -f, --for-real        Use the less-specific non-test filter."
       echo "  -h, --help            Show this help."
       echo "  -m, --map-reduce ARG  Specify which mapreduce file to use."
+      echo "  -o, --out ARG         Specify which file to output to."
       echo "  -p, --pull-data       Force the program to skip the cache, and pull the data again."
       echo "  -v, --verbose         Show more info about what's happening'."
       exit 0
@@ -37,6 +40,10 @@ while [[ $1 ]]; do
     -m|--map-reduce)
       shift
       MAPREDUCE=$1
+      ;;
+    -o|--out)
+      shift
+      OUTPUT=$1
       ;;
     -p|--pull-data)
       PULL=""
@@ -52,6 +59,6 @@ while [[ $1 ]]; do
 done
 
 if $VERBOSE; then
-  echo python -m mapreduce.job ../$MAPREDUCE.py --input-filter ../$FILTER --num-mappers 16 --num-reducers 4 --data-dir ../work/cache --work-dir ../work --output ../my_mapreduce_results.out $KEY $SECRET $PULL --bucket "telemetry-published-v1"
+  echo python -m mapreduce.job ../$MAPREDUCE.py --input-filter ../$FILTER --num-mappers 16 --num-reducers 4 --data-dir ../work/cache --work-dir ../work --output ../$OUTPUT $KEY $SECRET $PULL --bucket "telemetry-published-v1"
 fi
-python -m mapreduce.job ../$MAPREDUCE.py --input-filter ../$FILTER --num-mappers 16 --num-reducers 4 --data-dir ../work/cache --work-dir ../work --output ../my_mapreduce_results.out $KEY $SECRET $PULL --bucket "telemetry-published-v1"
+python -m mapreduce.job ../$MAPREDUCE.py --input-filter ../$FILTER --num-mappers 16 --num-reducers 4 --data-dir ../work/cache --work-dir ../work --output ../$OUTPUT $KEY $SECRET $PULL --bucket "telemetry-published-v1"
