c3bf02f1973d90b1e1b72a0cab73efad58e66189
Blake Winton <bwinton@latte.ca>
2014-05-20 11:20:46 -0400

    Clean up the python a little. (Make it pep8-compliant, 4-spaces for indents, print errors to stderr, make lines less than 80 characters, merge some if/elses…)

--- telemetry-server/analysis/process_output.py
+++ telemetry-server/analysis/process_output.py
@@ -1,7 +1,7 @@
-''' 
+'''
 post-process what comes out of m/r job
 
-usage: cat final_data/my_mapreduce_results_pre.out | python process_output.py 
+usage: cat final_data/my_mapreduce_results_pre.out | python process_output.py
 
 input: output from uitour, ex:
 visibleTabs--[221, 1] count	1
@@ -16,45 +16,41 @@ bookmarksBarEnabled,False,194609.0,0.5405
 bookmarksBarEnabled,True,165417.0,0.4595
 '''
 
-import fileinput
 from collections import defaultdict
-from pprint import pprint as pp
+import fileinput
 import operator
+from pprint import pprint as pp
+import sys
 
 features = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
 instances = None
 
 for line in fileinput.input():
-	if line.startswith("ERROR"):
-		continue
-	if line.startswith("click"):
-		tokens = line.split("-", 1)
-	elif line.startswith("customize"):
-		tokens = line.split("-", 1)
-	elif line.startswith("bucket") or line.startswith("__DEFAULT__"):
-		tokens = line.split("-c", 1)
-		tokens[1] = "c" + tokens[1]
-		del tokens[0]
-		tokens = tokens[0].split("-", 1)
-	elif line.startswith("seenPage"):
-		tokens = line.split("-",1)
-	else:
-		tokens = line.split("--", 1)
-	category = tokens[0]
-	if not category.startswith("instances"):
-		obj = tokens[1].split(" ", 1)[0]
-		val = tokens[1].split()
-		if val[1] == "sum":
-			features[category][obj]["sum"] = float(val[2])
-		elif val[1] == "count":
-			features[category][obj]["count"] = float(val[2])
-	else:
-		instances = float(tokens[0].split()[-1])
+    if line.startswith("ERROR"):
+        print >> sys.stderr, line
+    elif (line.startswith("click") or
+          line.startswith("customize") or
+          line.startswith("seenPage")):
+        tokens = line.split("-", 1)
+    elif line.startswith("bucket") or line.startswith("__DEFAULT__"):
+        tokens = line.split("-c", 1)
+        tokens[1] = "c" + tokens[1]
+        del tokens[0]
+        tokens = tokens[0].split("-", 1)
+    else:
+        tokens = line.split("--", 1)
+    category = tokens[0]
+    if not category.startswith("instances"):
+        obj = tokens[1].split(" ", 1)[0]
+        val = tokens[1].split()
+        if val[1] in ("sum", "count"):
+            features[category][obj][val[1]] = float(val[2])
+    else:
+        instances = float(tokens[0].split()[-1])
 
 print "item,subitem,avg. instances per session,pct sessions with occurrence"
 for category, cat_fs in features.iteritems():
-	for f in sorted(cat_fs,key = cat_fs.get, reverse = True):
-		print category + "," + f + "," +str(round(cat_fs[f]["sum"]/instances, 4))+"," +str(round(cat_fs[f]["count"]/instances, 4))
-
-
-
+    for f in sorted(cat_fs, key=cat_fs.get, reverse=True):
+        avg = str(round(cat_fs[f]["sum"]/instances, 4))
+        pct = str(round(cat_fs[f]["count"]/instances, 4))
+        print category + "," + f + "," + avg + "," + pct
--- telemetry-server/analysis/process_output_diffs.py
+++ telemetry-server/analysis/process_output_diffs.py
@@ -1,49 +1,41 @@
-import fileinput
 from collections import defaultdict
-from pprint import pprint as pp
+import fileinput
 import operator
+from pprint import pprint as pp
+import sys
 
 features = defaultdict(lambda: defaultdict(int))
 instances = {}
 
 
-
 for line in fileinput.input():
-	pre_file = fileinput.filename().endswith("pre.out")
-	if line.startswith("ERROR"):
-		continue
-	if line.startswith("visibleTabs") or line.startswith("hiddenTabs"):
-		continue
-	elif line.startswith("bucket"):
-		tokens = line.split("-c", 1)
-		tokens[1] = "c" + tokens[1]
-		line = tokens[1]
+    prepost_key = "post"
+    if fileinput.filename().endswith("pre.out"):
+        prepost_key = "pre"
+    if line.startswith("ERROR"):
+        print >> sys.stderr, line
+    if line.startswith("visibleTabs") or line.startswith("hiddenTabs"):
+        continue
+    elif line.startswith("bucket"):
+        tokens = line.split("-c", 1)
+        tokens[1] = "c" + tokens[1]
+        line = tokens[1]
 
-	if not line.startswith("instances"):
-		tokens = line.split()
-		item = tokens[0]
-		agg = tokens[1]
-		val = tokens[2]
-
-		if agg == "count":
-			#what's the way of doing this??
-			if pre_file:
-				features[item]["pre"] = float(val)
-			else:
-				features[item]["post"] = float(val)
-	else:
-		if pre_file:
-			instances["pre"] = float(line.split()[-1])
-		else:
-			instances["post"] = float(line.split()[-1])
+    if not line.startswith("instances"):
+        (item, agg, val) = line.split()[:3]
+        if agg == "count":
+            features[item][prepost_key] = float(val)
+    else:
+        instances[prepost_key] = float(line.split()[-1])
 
 for f in sorted(features):
-	if "pre" in features[f].keys() and "post" in features[f].keys():
-		pre = round(features[f]["pre"]*100/instances["pre"], 2)
-		post = round(features[f]["post"]*100/instances["post"], 2)
-		diff = post - pre
-		if diff >= 10 or (pre > 0 and diff/pre >= .5):
-			marker = "***"
-		else:
-			marker = ""
-		print marker, f, "\t", str(pre)+"%", str(post) + "%", "\t", str(post - pre) + "%"
+    if "pre" in features[f].keys() and "post" in features[f].keys():
+        pre = round(features[f]["pre"] * 100 / instances["pre"], 2)
+        post = round(features[f]["post"] * 100 / instances["post"], 2)
+        diff = post - pre
+        if diff >= 10 or (pre > 0 and diff/pre >= .5):
+            marker = "***"
+        else:
+            marker = ""
+        print (marker, f, "\t", str(pre)+"%", str(post) + "%",
+               "\t", str(post - pre) + "%")
